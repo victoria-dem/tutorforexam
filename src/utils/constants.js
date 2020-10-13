f3fed140d53b3fcb8a353005cb3c4cc7bad286ff
Mikhail Lisus <lisusmikhail@gmail.com>
2020-10-13 17:18:07 -0400

    stable_version_with_api_numbers

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
-export const startAnalogiesButton = document.querySelector('.quizes__start-analogies');
-export const startMathButton = document.querySelector('.quizes__start-math');
+export const startAnalogiesButton = document.querySelector('.quizzes__start-analogies');
+export const startMathButton = document.querySelector('.quizzes__start-math');
 export const submitAnswer = document.querySelector('.quiz__submit-answer');
 export const form = document.querySelector('.quiz');
-export const fullQuiz = document.querySelector('.quizes');
+export const fullQuiz = document.querySelector('.quizzes');
 export const nextQuestion = document.querySelector('.quiz__next-question');
 export const quizResultElement = fullQuiz.querySelector('.quiz__result');
 export const quizResultCheckmarkElement = fullQuiz.querySelector('.quiz__img');
