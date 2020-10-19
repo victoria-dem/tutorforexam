4fb9abc98d6b4aa2a9420d6e9ddc4cd2211843db
Mikhail Lisus <lisusmikhail@gmail.com>
2020-10-19 12:05:43 -0400

    account_score_menu_classes

--- src/utils/glancecards.js
+++ src/utils/glancecards.js
@@ -1,18 +1,22 @@
+import firstImg from './../images/choose1.webp';
+import secondImg from './../images/choose2.webp';
+import thirdImg from './../images/choose3.webp';
+
 export const glanceCards = [
     {
-        link: "images/choose_one.webp",
+        link: firstImg,
         alt: "Square anthropomorphic characters playing a math game",
         title: "Math and Analogies",
         text: "More than 500 SSAT & ISEE test questions and problems with tutorials based on 10 levels of difficulty"
     },
     {
-        link: "images/choose2.webp",
+        link: secondImg,
         alt: "Square character shredding list to digits",
         title: "Different topics",
         text: "More than 100 topics for students to learn and excel at."
     },
     {
-        link: "images/choose3.webp",
+        link: thirdImg,
         alt: "Square character trying to solve problem",
         title: "Help is here",
         text: "More than 100 clues to guide you to a better way to solve a problem"
