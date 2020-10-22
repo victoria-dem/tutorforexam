05209b7cd909f5d86ef10eabc2119ea7f4d13429
Mikhail Lisus <lisusmikhail@gmail.com>
2020-10-21 22:42:44 -0400

    refactor: style improvment

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -16,6 +16,10 @@ export class Popup {
         this._popupContainer.classList.add("popup_fade_out");
         window.removeEventListener("keyup", this._handleEscClose);
         this._popupContainer.querySelector(".form").reset();
+        this._popupContainer.querySelectorAll('.form__field').forEach(inputElement =>{
+            console.log('nice try');
+            inputElement.textContent = '';
+        })
     }
     
     _handleClickClose(evt) {
